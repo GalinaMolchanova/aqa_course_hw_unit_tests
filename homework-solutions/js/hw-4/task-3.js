d64f595893f7746d3485aa1ea9e3ca5a1cfda0fb
gmolchanova <gmolchanova@tradingview.com>
2025-02-16 20:48:43 +0300

    HW4

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -6,3 +6,21 @@
   преобразовываясь в number
 
 */
+
+const minAge = 18;
+
+const maxAge = 60;
+
+let age = 60;
+
+if (isNaN(age) || !age) {
+  console.log(age + ' is not a number')
+} else if(age < minAge) {
+console.log("You don\'t have access cause your age is " + age + " It\'s less then " + minAge)
+} else if(age >= 18 && age < maxAge) {
+console.log("Welcome")
+} else if (age >= maxAge) {
+console.log("Keep calm and look Culture channel")
+} else {
+console.log("Technical work")
+};
